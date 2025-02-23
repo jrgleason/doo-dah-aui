a321cf1b9d389616897ddde1da1f6bc53d9b9acc
jrgleason <jackiegleason@gmail.com>
2025-02-23 18:30:01 -0500

    Adding the rest

---
+++ app/src/main/java/doo/dah/aui/artificial_unintelligence/ArtificialUnIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package doo.dah.aui.artificial_unintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtificialUnIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtificialUnIntelligenceApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/doo/dah/aui/artificial_unintelligence/ArtificialUnIntelligenceApplicationTests.java
@@ -0,0 +1,15 @@
+package doo.dah.aui.artificial_unintelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ArtificialUnIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
