59df18e8505b04321c49666476f643671ec5da11
Jackie Gleason <jackiegleason@gmail.com>
2025-04-11 21:06:35 -0400

    Fix stuff (#4)
    * Getting the AI features working (#3)
    
    * Token provider is working now
    
    * Everything is basically working even elevated privileges
    
    * Fixing things

--- app/src/main/java/doo/dah/aui/artificial_unintelligence/config/AiConfig.java
+++ app/src/main/java/doo/dah/aui/artificial_unintelligence/config/AiConfig.java
@@ -4,7 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,19 +21,19 @@ public class AiConfig {
         return new MessageChatMemoryAdvisor(new InMemoryChatMemory());
     }
 
-    @Bean(name = "openAiBuildClient")
+    @Bean
     public ChatClient buildClient(
-            ChatClient.Builder openAiBuilder,
+            ChatClient.Builder aiBuilder,
             MessageChatMemoryAdvisor messageChatMemoryAdvisor,
             VectorStore pineconeVectorStore
     ) {
-        return openAiBuilder
+        return aiBuilder
                 .defaultAdvisors(
                         messageChatMemoryAdvisor,
                         new QuestionAnswerAdvisor(pineconeVectorStore)
                 )
                 .defaultSystem(instructions)
-                .defaultOptions(new OpenAiChatOptions())
+                .defaultOptions(new OllamaOptions())
                 .build();
     }
 }
